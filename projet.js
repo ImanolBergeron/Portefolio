d17338b04c516547f6489a5226fc6fe3c2ee4132
Imanol <imanollol83@gmail.com>
2025-01-10 22:53:56 +0100

    affichage des projet modifier et image temporaire pour projet

--- projet.js
+++ projet.js
@@ -1,23 +1,37 @@
 const Projet = [
-    {name : "Pacman", srcImg : ""},
-    {name : "2048", srcImg : ""},
-    {name : "Forgotten", srcImg : ""},
-    {name : "Apexdle", srcImg : ""},
-    {name : "CarbonTracker", srcImg : ""},
-    {name : "Unideckbuilders", srcImg : ""},
-    {name : "Labyrinth", srcImg : ""},
-    {name : "Towa", srcImg : ""},
-    {name : "Odomo", srcImg : ""}
+    {name : "Pacman", srcImg : "imageProjet/Pacman.png", techno : "Python"},
+    {name : "2048", srcImg : "imageProjet/2048.png", techno : "Python"},
+    {name : "Forgotten", srcImg : "imageProjet/Forgotten.png", techno : "C#/Unity"},
+    {name : "Apexdle", srcImg : "imageProjet/Apexdle.png", techno : "JS"},
+    {name : "CarbonTracker", srcImg : "imageProjet/CarbonTracker.png", techno : "C#"},
+    {name : "Unideckbuilders", srcImg : "imageProjet/Unideckbuilders.png", techno : "C#"},
+    {name : "Labyrinth", srcImg : "imageProjet/Labyrinth.png", techno : "Java"},
+    {name : "Towa", srcImg : "imageProjet/Towa.jpg", techno : "Java"},
+    {name : "Odomo", srcImg : "imageProjet/Odomo.jpg", techno : "Java"}
 ]
-const DIVPROJET = document.getElementById("projets");
+const DIVPROJET = document.getElementById("tableProjet");
 
-Projet.forEach( proj =>{
+for(i=0;i<Projet.length;i++){
+    var a = i;
+    if( a % 3 === 0){
+        var ligne = document.createElement("tr");
+    }
+    var col = document.createElement("td");
     var link = document.createElement("a");
     link.href = "Presentation.html";
     link.className="explore-linkFront";
    img = document.createElement("img");
-   img.alt = proj.name;
-   img.src = proj.srcImg;
+   img.className="ImageProjet";
+   img.alt = Projet[i].name;
+   img.src = Projet[i].srcImg;
    link.appendChild(img);
-   DIVPROJET.appendChild(link);
-});
+   var Titre = document.createElement("p");
+   Titre.textContent = Projet[i].name + " - " + Projet[i].techno;
+   link.appendChild(Titre);
+   col.appendChild(link);
+   ligne.appendChild(col);
+   if( (i+1) % 3 === 0){
+    DIVPROJET.appendChild(ligne);
+    }
+   
+}
